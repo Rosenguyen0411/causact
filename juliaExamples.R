a1fe945364fc28c9def7b565cb7a75739ecf65fb
Nhung Nguyen <nguyenthihongnhung0401@gmail.com>
2019-07-02 20:48:26 -0400

    a

--- juliaExamples.R
+++ juliaExamples.R
@@ -35,7 +35,7 @@ graph = dag_create() %>%
             nodeLabels = "theta",  
             addDataNode = TRUE)
 graph %>% dag_render()
-graph %>% dag_greta()
+#graph %>% dag_greta()
 graph %>% dag_julia(NUTS = TRUE)
 graph %>% dag_julia(HMC = TRUE)
 
@@ -122,7 +122,55 @@ graph = dag_create() %>%
             addDataNode = TRUE)
 
 graph %>% dag_render()
-graph %>% dag_greta()
+#graph %>% dag_greta()
+graph %>% dag_julia(NUTS= TRUE)
+graph %>% dag_julia(HMC= TRUE)
+
+
+############# Statistical Rethinking - Rugged model (m8.5), linear regression of log_gdp on rugged, 
+############## for african and non-african countries
+library(rethinking)
+data(rugged)
+d <- rugged
+
+# make log version of outcome
+d$log_gdp <- log( d$rgdppc_2000 )
+# extract countries with GDP data
+dd <- d[ complete.cases(d$rgdppc_2000) , ]
+
+# rescale variables
+log_gdp_std <- dd$log_gdp / mean(dd$log_gdp)
+rugged_std <- dd$rugged / max(dd$rugged)
+# make variable to index Africa (1) or not (2)
+cid <- ifelse( dd$cont_africa==1 , 1 , 2 )
+
+
+graph = dag_create() %>%
+  dag_node(descr = "log gdp", label = "log_gdp",
+           rhs = normal(mu, sigma),
+           data = log_gdp_std) %>%
+  dag_node(descr = "Variation", label = "sigma",
+           rhs = exponential(1),
+           child = "log_gdp") %>%
+  dag_node(descr = "Mean", label = "mu",
+           rhs = a + b * (rugged - 0.215),
+           child = "log_gdp")  %>%
+  dag_node(descr = "ruggedness", label = "rugged",
+           data = rugged_std,
+           child = "mu") %>%
+  dag_node(descr = "intercept", label = "a",
+           rhs = normal(1, 0.1),
+           child = "mu") %>%
+  dag_node(descr = "slope", label = "b",
+           rhs = normal(0, 0.3),
+           child = "mu") %>%
+  dag_plate(descr = "African index", label = "cid",
+            data = cid,
+            nodeLabels = c("b", "a"),
+            addDataNode = TRUE)
+
+graph %>% dag_render()
+#graph %>% dag_greta()
 graph %>% dag_julia(NUTS= TRUE)
 graph %>% dag_julia(HMC= TRUE)
 
